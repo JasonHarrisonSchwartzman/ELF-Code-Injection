ade7b308a018e6a32860fed2502784e13979d9be
JasonHarrisonSchwartzman <megaslowbro1@gmail.com>
2023-10-25 02:32:19 +0000

    added more stuff

--- section.py
+++ section.py
@@ -92,6 +92,25 @@ def modify_file(original_file,offset,data):
     original_file.seek(offset)
     original_file.write(data)
 
+def edit_rela_plt_section(elf_object,original_file,size):
+    rela_plt_section = elf_object.get_section(11)
+    rela_plt_section_offset = 0x608
+    rela_plt_relo_size = 24
+    rela_plt_num_relocations = rela_plt_section.num_relocations()
+    for i in range(rela_plt_num_relocations):
+        rela_plt_offset = rela_plt_section.get_relocation(i)['r_offset']
+        modify_file(original_file,rela_plt_section_offset + i * rela_plt_relo_size,(rela_plt_offset+size).to_bytes(8,'little'))
+
+def edit_rela_dyn_section(elf_object,original_file,size):
+    rela_dyn_section = elf_object.get_section(10)
+    rela_dyn_section_offset = 0x548
+    rela_dyn_relo_size = 24
+    rela_dyn_num_relocations = rela_dyn_section.num_relocations()
+    for i in range(rela_dyn_num_relocations):
+        rela_dyn_offset = rela_dyn_section.get_relocation(i)['r_offset']
+        modify_file(original_file,rela_dyn_section_offset + i * rela_dyn_relo_size,(rela_dyn_offset+size).to_bytes(8,'little'))
+
+
 def edit_dynamic_section(elf_object,original_file,size):
     dynamic_section_offset = 0x2dd0
     dynamic_section = elf_object.get_section(23)
@@ -102,7 +121,7 @@ def edit_dynamic_section(elf_object,original_file,size):
     for i in range(dynamic_section.num_tags()):
         tag = dynamic_section._get_tag(i)
         tag_value = tag['d_ptr']
-        if (ENUM_D_TAG_COMMON[tag['d_tag']] < 30 and tag['d_ptr'] < 0x1169):
+        if (ENUM_D_TAG_COMMON[tag['d_tag']] < 30 and tag['d_ptr'] > 0x1169):
             print(tag['d_tag'],dynamic_section._get_tag(i)['d_ptr'])
             modify_file(original_file,dynamic_section_offset + 16 * i + 8,(tag_value+size).to_bytes(8,'little'))
 
@@ -167,15 +186,14 @@ def edit_symbol_table(elf_object,original_file,inject_offset,size):
     #modify_file(original_file,sym_tab_offset + sym_size * 18 + value_offset,(sym_5+size).to_bytes(8,'little'))
     #modify_file(original_file,sym_tab_offset + sym_size * 57 + value_offset,(sym_6+size).to_bytes(8,'little'))
     print(sym_tab_offset)
-    print(sym_1)
-    print(sym_2)
 
 def edit_elf_section(elf_object,original_file,elf_section,section_index,size):
     
     section_header_table = elf_object['e_shoff']
     section_header_size = elf_object['e_shentsize']
     section_offset_offset = 24
-    section_size_offset = 32
+    section_addr_offset = 16
+    total_section_addr_offset = section_header_table + section_header_size * section_index + section_addr_offset
     total_section_offset_offset = section_header_table + section_header_size * section_index + section_offset_offset
     total_section_size_offset = total_section_offset_offset + 8
     print("section index " + str(section_index))
@@ -184,28 +202,50 @@ def edit_elf_section(elf_object,original_file,elf_section,section_index,size):
     print("total section offset offset " + str(total_section_offset_offset))
                                             # !! IMPORTANT !!
     section_offset = elf_section['sh_offset'] # NOT sh_addr. sh_addr is the logical address of the section
-    section_size = elf_section['sh_size']
-
+    section_addr = elf_section['sh_addr']
     #changing offset
     modify_file(original_file,total_section_offset_offset,(size + section_offset).to_bytes(8,'little'))
 
-    #changing size
-    #modify_file(original_file,total_section_size_offset,(size + section_size).to_bytes(8,'little'))
+    #changing virtual address
+    modify_file(original_file,total_section_addr_offset,(size + section_addr).to_bytes(8,'little'))
 
     assert(original_file.tell() <= section_header_table + section_header_size * (section_index + 1)) # You've written outside the section
 
+def edit_text_size(elf_object,original_file,size):
+    section_header_table = elf_object['e_shoff']
+    section_header_size = elf_object['e_shentsize']
+    section_size_offset = 32
+    text_index = 16
+    text_section = elf_object.get_section_by_name(".text")
+    text_section_size = text_section['sh_size']
+    total_section_size_offset = section_header_table + section_header_size * text_index + section_size_offset
+    modify_file(original_file,total_section_size_offset,(text_section_size+size).to_bytes(8,'little'))
 
 def edit_elf_sections(elf_object,original_file,section,size):
     """ Edits all sections by size after section
     """
+
     for i in range(elf_object.get_section_index(section)+1,elf_object.num_sections()):
         edit_elf_section(elf_object,original_file,elf_object.get_section(i),i,size)
+
+def edit_start(elf_object,original_file,size):
+    csu_fini_offset = 0x1096
+    modify_file(original_file,csu_fini_offset,(0xa6).to_bytes(1,'little'))
+    csu_init_offset = 0x109d
+    modify_file(original_file,csu_init_offset,(0x2f).to_bytes(1,'little'))
+    call_offset = 0x10aa
+    #modify_file(original_file,call_offset,(0x42).to_bytes(1,'little'))
 
 size = 16
 f = open('hello','r+b')
 elf = ELFFile(open('hello','rb'))
 modify_file(f,40,(elf['e_shoff']+16).to_bytes(8,'little'))
+edit_text_size(elf,f,size)
 edit_elf_sections( elf,f,'.text',size)
 edit_symbol_table(elf,f,0x1194,size)
 edit_program_header(elf,f,size)
 edit_dynamic_section(elf,f,size)
+#edit_rela_dyn_section(elf,f,size)
+#edit_rela_plt_section(elf,f,size)
+edit_start(elf,f,size)
+
